7f2f9c2374dd8e193dc820cb388f0c7278489432
afaf <shoog-alroo7_@hotmail.com>
2017-04-25 09:58:06 +0300

    setting

--- app/src/main/java/com/amakenapp/website/amakenapp/helper/AppCompatPreferenceActivity.java
+++ app/src/main/java/com/amakenapp/website/amakenapp/helper/AppCompatPreferenceActivity.java
@@ -1,31 +1,69 @@
 package com.amakenapp.website.amakenapp.helper;
 
-import android.content.res.Configuration;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatDelegate;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amakenapp.website.amakenapp.R;
 
 /**
  * A {@link android.preference.PreferenceActivity} which implements and proxies the necessary calls
  * to be used with AppCompat.
  */
-public abstract class AppCompatPreferenceActivity extends PreferenceActivity {
-
-    private AppCompatDelegate mDelegate;
+public abstract class AppCompatPreferenceActivity extends AppCompatActivity {
+    SharedPreferences sh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getDelegate().installViewFactory();
-        getDelegate().onCreate(savedInstanceState);
         super.onCreate(savedInstanceState);
+        sh= getSharedPreferences("Setting", Context.MODE_PRIVATE);
+        String myStyle = sh.getString("myStyle","Default setting");
+        CoordinatorLayout layte =(CoordinatorLayout) findViewById(R.layout.activity_add_place);
+            switch (myStyle){
+                case "Default setting":
+                    this.setTheme(R.style.AppTheme);
+                    break;
+                case "style 1":
+                    this.setTheme(R.style.AppTheme1);
+                    break;
+                case "style 2":
+                    this.setTheme(R.style.AppTheme2);
+                    break;
+    }}
+/** @Override
+    protected void onStart(){
+        super.onStart();
+        if (sh.getBoolean("enable_settings",false)){
+            switch (sh.getString("theme","Default setting")){
+                case "Default setting":
+                    this.setTheme(R.style.AppTheme);
+                    break;
+                case "style 1":
+                    this.setTheme(R.style.AppTheme1);
+                    break;
+                case "style 2":
+                    this.setTheme(R.style.AppTheme2);
+                    break;
+
+            }
+        }}*/
+       public void MyStyle(String myStyle){
+        SharedPreferences.Editor editor= sh.edit();
+        editor.putString("myStyle",myStyle);
+        editor.apply();
+        finish();
+
     }
+
+    }
+
+
+
+    /**
+     * A {@link android.preference.PreferenceActivity} which implements and proxies the necessary calls
+     * to be used with AppCompat.
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -107,3 +145,5 @@ public abstract class AppCompatPreferenceActivity extends PreferenceActivity {
         return mDelegate;
     }
 }
+
+     */
